e66e95c685888f6d301d0694d1d4e3d0d9ebbb2d
daniel.yoali <daniel.yoali@gopay.co.id>
2026-02-24 15:03:27 +0700

    Initial Commit

---
+++ SwiftFundamentals/SwiftFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFundamentalsApp.swift
+//  SwiftFundamentals
+//
+//  Created by Daniel Agustian Yoali on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftFundamentalsTests/SwiftFundamentalsTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftFundamentalsTests.swift
+//  SwiftFundamentalsTests
+//
+//  Created by Daniel Agustian Yoali on 24/02/26.
+//
+
+import Testing
+
+struct SwiftFundamentalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
